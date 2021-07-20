cfae77ad8fca710cff5eba7b0513edcf6af8a1b7
Christian Knabenhans <christian@knabenhans.fr>
2021-07-20 19:31:44 +0200

    Rename module, Use big.Int arithmetic, Use array of Z_qi hashes

---
+++ examples/bfv/homomorphic_hash/hash_test.go
@@ -0,0 +1,106 @@
+package homomorphic_hash
+
+import (
+	"github.com/ldsec/lattigo/v2/bfv"
+	"github.com/ldsec/lattigo/v2/rlwe"
+	"github.com/ldsec/lattigo/v2/utils"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func getParams() bfv.Parameters {
+	// Set up FV parameters
+	paramsDef := bfv.PN13QP218
+	paramsDef.T = 0x3ee0001
+
+	params, err := bfv.NewParametersFromLiteral(paramsDef)
+	if err != nil {
+		panic(err)
+	}
+	return params
+}
+
+func getPRNG() utils.PRNG {
+	prng, err := utils.NewKeyedPRNG([]byte{42})
+	if err != nil {
+		panic(err)
+	}
+	return prng
+}
+
+func TestHasher_Hash(t *testing.T) {
+	params := getParams()
+	encoder := bfv.NewEncoder(params)
+	kgen := bfv.NewKeyGenerator(params)
+	sk := kgen.GenSecretKey()
+	encryptorSk := bfv.NewEncryptor(params, sk)
+
+	hasher := NewHasher(params)
+
+	msg := make([]uint64, 1<<params.LogN())
+	for i := uint64(0); i < uint64(len(msg)); i++ {
+		msg[i] = i
+	}
+	ptxt := bfv.NewPlaintext(params)
+	encoder.EncodeUint(msg, ptxt)
+
+	// Test if Hash() is deterministic for the same key (alpha, beta)
+	ctxt := encryptorSk.EncryptNew(ptxt)
+	h1 := hasher.Hash(ctxt)
+	h2 := hasher.Hash(ctxt)
+
+	require.Equal(t, h1, h2)
+
+	// Test if Hash() is universal one-way
+	for i := 0; i < 100; i++ {
+		encryptorSk.Encrypt(ptxt, ctxt)
+		hNew := hasher.Hash(ctxt)
+		require.NotEqual(t, h1, hNew)
+	}
+}
+
+func TestHash_Add(t *testing.T) {
+	// Test if Hash() is homomorphic w.r.t. addition
+	params := getParams()
+	prng := getPRNG()
+	evaluator := bfv.NewEvaluator(params, rlwe.EvaluationKey{})
+	hasher := NewHasher(params)
+
+	for i := 0; i < 1; i++ {
+		c1 := bfv.NewCiphertextRandom(prng, params, 1)
+		c2 := bfv.NewCiphertextRandom(prng, params, 1)
+
+		h1 := hasher.Hash(c1)
+		h2 := hasher.Hash(c2)
+
+		hAdd := h1.Add(h2)
+		cAdd := evaluator.AddNew(c1, c2)
+		require.Equal(t, hAdd, hasher.Hash(cAdd))
+	}
+}
+
+func TestHash_Mul(t *testing.T) {
+	// Test if Hash() is homomorphic w.r.t. multiplications
+	params := getParams()
+	prng := getPRNG()
+
+	kgen := bfv.NewKeyGenerator(params)
+	sk := kgen.GenSecretKey()
+	rk := kgen.GenRelinearizationKey(sk, 1)
+	ek := rlwe.EvaluationKey{Rlk: rk}
+	evaluator := bfv.NewEvaluator(params, ek)
+	hasher := NewHasher(params)
+
+	for i := 0; i < 1; i++ {
+		c1 := bfv.NewCiphertextRandom(prng, params, 1)
+		c2 := bfv.NewCiphertextRandom(prng, params, 1)
+
+		h1 := hasher.Hash(c1)
+		h2 := hasher.Hash(c2)
+
+		hMul := h1.Mul(h2)
+		cMul := evaluator.RelinearizeNew(evaluator.MulNew(c1, c2))
+		//cMul := evaluator.MulNew(c1, c2)
+		require.Equal(t, hMul, hasher.Hash(cMul))
+	}
+}
---
+++ examples/bfv/homomorphic_hash/main.go
@@ -0,0 +1,112 @@
+// Package homomorphic_hash implements homomorphic hash functions for FV ciphertexts as introduced by Fiore et al.
+// in "Efficiently Verifiable Computation on Encrypted Data".
+package homomorphic_hash
+
+import (
+	"github.com/ldsec/lattigo/v2/bfv"
+	"github.com/stretchr/testify/require"
+	"math/big"
+)
+
+type Hasher interface {
+	Hash(*bfv.Ciphertext) *hash
+}
+
+type hasher struct {
+	params bfv.Parameters
+	alpha  uint64
+	beta   uint64
+}
+
+type Hash interface {
+	Add(*Hash) *Hash
+	Mul(*Hash) *Hash
+}
+
+type hash struct {
+	Values []*big.Int
+	Q      []uint64
+}
+
+func (h1 *hash) Add(h2 *hash) *hash {
+	require.Equal(nil, h1.Q, h2.Q)
+
+	res := make([]*big.Int, len(h1.Q))
+	for k := 0; k < len(res); k++ {
+		res[k] = big.NewInt(0)
+		res[k] = res[k].Add(h1.Values[k], h2.Values[k])
+		res[k] = res[k].Mod(res[k], big.NewInt(int64(h1.Q[k])))
+	}
+
+	return &hash{res, h1.Q}
+}
+
+func (h1 *hash) Mul(h2 *hash) *hash {
+	require.Equal(nil, h1.Q, h2.Q)
+
+	res := make([]*big.Int, len(h1.Q))
+	for k := 0; k < len(res); k++ {
+		res[k] = big.NewInt(0)
+		res[k] = res[k].Mul(h1.Values[k], h2.Values[k])
+		res[k] = res[k].Mod(res[k], big.NewInt(int64(h1.Q[k])))
+	}
+
+	return &hash{res, h1.Q}
+}
+
+func NewHasher(params bfv.Parameters) Hasher {
+	//prng, err := utils.NewPRNG()
+	//if err != nil {
+	//	panic(err)
+	//}
+	// TODO: Very inefficient (but correct) sampling from Z_q
+	//sampler := ring.NewUniformSampler(prng, params.RingQ())
+	alpha := uint64(0) //sampler.ReadNew().Coeffs[0][0]
+	beta := uint64(0)  //sampler.ReadNew().Coeffs[0][1]
+
+	return &hasher{params, alpha, beta}
+}
+
+// Hash maps a FV ciphertext (interpreted as a polynomial in Zq[X][Y]) to Zq
+// This is the simple construction (without bilinear pairings) that is homomorphic and universal one-way, but not collision-resistant.
+func (hasher *hasher) Hash(ctxt *bfv.Ciphertext) *hash {
+	// ctxt \in Z_q[X][Y]
+	N := ctxt.Value[0].Degree() // deg_X(ctxt)
+	c := ctxt.Degree()          // deg_Y(ctxt)
+	Q := hasher.params.Q()
+
+	//if c != 1 {
+	//	panic(nil)
+	//}
+
+	// hash = ctxt[X][Y] evaluated at Y = alpha, X = beta
+	// TODO: figure out order of exponents
+	hs := make([]*big.Int, len(Q))
+	for i := 0; i < len(hs); i++ {
+		hs[i] = big.NewInt(0)
+	}
+
+	for j := 0; j < c; j++ {
+
+		for i := 0; i < N; i++ {
+			for k := 0; k < len(Q); k++ {
+				alphaPowJ := big.NewInt(int64(hasher.alpha))
+				alphaPowJ = alphaPowJ.Exp(alphaPowJ, big.NewInt(int64(j)), big.NewInt(int64(Q[k])))
+
+				betaPowI := big.NewInt(int64(hasher.beta))
+				betaPowI = betaPowI.Exp(betaPowI, big.NewInt(int64(i)), big.NewInt(int64(Q[k])))
+
+				coeffIJK := big.NewInt(int64(ctxt.Value[j].Coeffs[k][i]))
+				m := coeffIJK
+				m = m.Mul(m, alphaPowJ)
+				m = m.Mul(m, betaPowI)
+				m = m.Mod(m, big.NewInt(int64(Q[k])))
+
+				hs[k] = hs[k].Add(hs[k], m)
+				hs[k] = hs[k].Mod(hs[k], big.NewInt(int64(Q[k])))
+			}
+		}
+	}
+
+	return &hash{hs, hasher.params.Q()}
+}
